ed002e01b4dd43203264864f05f45e9e7cfeeab1
lkanwvl <ddjjiy1204@naver.com>
2024-12-08 10:56:31 +0900

    스테미너작업

--- Assets/Script/Charactor/Actor_Player.cs
+++ Assets/Script/Charactor/Actor_Player.cs
@@ -1,22 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Actor_Player : Actor
 {
     Rigidbody rigid;
     Skill_Near skillNear;
     float gravity = -9.81f;
+    [SerializeField] float stamina;
+    [SerializeField] Slider staminaSlider;
+    [SerializeField] Slider staminaSliderBack;
     [SerializeField] float speed;
     [SerializeField] GameObject skillMgr;
     [SerializeField] Transform playerCam;
     Vector3 MoveDir = Vector3.zero;
+    [SerializeField] float staminaTime = 2;
+    float playerSpeed;
     void Awake()
     {
         rigid = GetComponent<Rigidbody>();
         skillNear = skillMgr.GetComponent<Skill_Near>();
     }
+
+    private void Start()
+    {
+        playerSpeed = speed;
+    }
+
     private void Update()
     {
         Move();
@@ -24,6 +34,8 @@ public class Actor_Player : Actor
         {
             skillNear.SkillPlay(eSkill.Normal_Near);
         }
+        staminaIdle(true);
+        staminaTimer(true, false);
     }
     void Move()
     {
@@ -42,7 +54,53 @@ public class Actor_Player : Actor
         cameraRight.y = 0f;
 
         MoveDir = MoveDir.z * cameraForward + MoveDir.x * cameraRight;
-
+        if(Input.GetKeyDown(KeyCode.LeftShift) && staminaSlider.value > 0)
+        {
+            speed *= 2;
+            staminaIdle(false);
+            if (staminaSlider.value <= 0)
+            {
+                return;
+            }
+        }
+        if
+        (Input.GetKey(KeyCode.LeftShift) && staminaSlider.value > 0)
+        {
+            staminaSlider.value -= stamina * Time.deltaTime;
+            staminaTimer(false, true);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift) && speed >= playerSpeed)
+        {
+            speed /= 2;
+        }
         transform.Translate(MoveDir * speed * Time.deltaTime, Space.World);
+    }
+
+    private void staminaIdle(bool _bool)
+    {
+        if(_bool == true && staminaTime == 0)
+        {
+            staminaSlider.value += stamina / 2 * Time.deltaTime;
+        }
+        else if(_bool == false)
+        {
+            return;
+        }
+    }
+
+    private void staminaTimer(bool _timer, bool _reset)
+    {
+        if(_timer == true)
+        {
+            staminaTime -= Time.deltaTime;
+        }
+        if(staminaTime <= 0)
+        {
+            staminaTime = 0;
+        }
+        if(_reset == true)
+        {
+            staminaTime = 2;
+        }
     }
 }
---
+++ Assets/Script/Tile/TileMgr.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMgr : MonoBehaviour
+{
+    [SerializeField] List<GameObject> tiles = new List<GameObject>();
+}
